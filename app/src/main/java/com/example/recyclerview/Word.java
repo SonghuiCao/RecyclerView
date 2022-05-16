28d03098aa3cadb961c0be7ce1085b86414d5837
SonghuiCao <96573592+SonghuiCao@users.noreply.github.com>
2022-05-16 10:56:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Word.java
@@ -0,0 +1,19 @@
+package com.example.recyclerview;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import androidx.annotation.ColorLong;
+import androidx.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+    public Word(@NonNull String word){this.mWord = word; }
+    public String getmWord(){return this.mWord; }
+}
